d4e13b0768a08b189dec671118a604c5f5d1865d
Philipocoding <145617802+Philipocoding@users.noreply.github.com>
2024-07-03 12:04:30 +0100

    Solution accepted

--- Two Sum /Two_Sum.cs
+++ Two Sum /Two_Sum.cs
@@ -1,1 +1,19 @@
-
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        
+        int [] result = new int[2];
+        
+        for(int i = 0; i < nums.Length - 1; i++)
+        {
+            for(int ii = 1; ii < nums.Length; ii++)
+            {
+                if(nums[ii] + nums[i] == target){
+                    result[0] = i;
+                    result[1] = ii;
+                    
+                }            
+            }
+        }
+        return result;
+    }
+}
